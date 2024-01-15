429962a5fd2cbe5c1d3852068a0934dce16f41d5
gagan3512 <gagan1825.cse18@chitkara.edu.in>
2024-01-14 23:10:46 -0500

    code added

---
+++ ex1_test.py
@@ -0,0 +1,5 @@
+import gagan as name_point
+
+def test_hi_my_name_is():
+    assert len(name_point.hi_my_name_is())  > 1 
+
---
+++ gagan.py
@@ -0,0 +1,2 @@
+def hi_my_name_is():
+    return "Gagan" # Write your name inside the quotation marks
